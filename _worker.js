cd4ea09f99dd8c6bb5d45a3d9c3235670bd756bc
mfbpn <141901587+mfbpn@users.noreply.github.com>
2024-07-31 22:34:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -69,7 +69,7 @@ export default {
 		MainData = 自建节点;
 		urls = await ADD(订阅链接);
 
-		if ( !(token == mytoken || token == fakeToken || url.pathname == ("/api/v1/client/"+ mytoken) || url.pathname.includes("/api/v1/client/"+ mytoken + "?")) ) {
+		if ( !(token == mytoken || token == fakeToken || url.pathname == ("/api/v1/client/subscribe"+ mytoken) || url.pathname.includes("/api/v1/client/subscribe"+ mytoken + "?")) ) {
 			if ( TG == 1 && url.pathname !== "/" && url.pathname !== "/favicon.ico" ) await sendMessage(`#异常访问 ${FileName}`, request.headers.get('CF-Connecting-IP'), `UA: ${userAgent}</tg-spoiler>\n域名: ${url.hostname}\n<tg-spoiler>入口: ${url.pathname + url.search}</tg-spoiler>`);
 			const envKey = env.URL302 ? 'URL302' : (env.URL ? 'URL' : null);
 			if (envKey) {
