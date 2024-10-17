a52e5d142e657be3e1da1437167cd6098005ed32
mfbpn <141901587+mfbpn@users.noreply.github.com>
2024-10-17 13:46:27 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ https://sub.xf.free.hr/auto
 `
 
 let urls = [];
-#let subconverter = "subapi.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+//let subconverter = "subapi.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconverter = "pub-api-1.bianyuan.xyz"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
